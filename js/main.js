5c32eb7e8d6b59ad4ac101bf42c8ba32367d93dc
Luke Griffith <luketgriffith@gmail.com>
2015-10-08 11:37:13 -0400

    brought over etsy images and titles from JS file

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-// var firstItem= etsy.results[0];
+
 var templateString = $('#itemTemplate').text()      // set var templateString to the text inside of $('#itemTemplate')-- #itemTemplate is the template on HTML page
 var templateFunction = _.template(templateString);  //make function of templateString with _.template
 _.each(etsy.results, function(item){    //for each 'results' in etsy object, do this to (item):
   var itemHTML= templateFunction(item);  //create var itemHTML which equals the return of templateFunction(item);
-  // Turn this Line back on!!// $('.wrapper').append(itemHTML);     // appending itemHTML to the end of $('.wrapper')
+  $('.allstar').append(itemHTML); 
+  console.log(itemHTML);    // appending itemHTML to the end of $('.wrapper')
 })
-// var img= _.pluck(etsy.results.Images, 'url_75x75');
 
+// var numberString= $('#numTemplate').text();
+
+var count= etsy.count;
+$('.numberResults').append(count+' Results');
