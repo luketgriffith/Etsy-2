824810c7e00296cc9d5fee8ff4ab99da52d52def
Luke Griffith <luketgriffith@gmail.com>
2015-10-07 22:24:12 -0400

    styling sidebar

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var templateString = $('#itemTemplate').text()      // set var templateString to
 var templateFunction = _.template(templateString);  //make function of templateString with _.template
 _.each(etsy.results, function(item){    //for each 'results' in etsy object, do this to (item):
   var itemHTML= templateFunction(item);  //create var itemHTML which equals the return of templateFunction(item);
-  $('.wrapper').append(itemHTML);     // appending itemHTML to the end of $('.wrapper')
+  // Turn this Line back on!!// $('.wrapper').append(itemHTML);     // appending itemHTML to the end of $('.wrapper')
 })
 // var img= _.pluck(etsy.results.Images, 'url_75x75');
 
