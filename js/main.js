9f3747adaaf57c9460b3c69f4ab3964c6d574b54
Luke Griffith <luketgriffith@gmail.com>
2015-10-08 14:46:41 -0400

    styling bottom of page

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@ var templateString = $('#itemTemplate').text()      // set var templateString to
 var templateFunction = _.template(templateString);  //make function of templateString with _.template
 _.each(etsy.results, function(item){    //for each 'results' in etsy object, do this to (item):
   var itemHTML= templateFunction(item);  //create var itemHTML which equals the return of templateFunction(item);
-  $('.allstar').append(itemHTML); 
+  $('.heynow').append(itemHTML); 
       // appending itemHTML to the end of $('.wrapper')
 })
 
 // var numberString= $('#numTemplate').text();
+var count= etsy.count;
+$('.allstar').append('('+ count+' Results'+')');
 var keywords= etsy.params.keywords;
 console.log(keywords);
-$('.heynow').append('"'+keywords+'"');
-var count= etsy.count;
-$('.heynow').append('('+ count+' Results'+')');
+$('.keyword').append('"'+keywords+'"');
+
  
